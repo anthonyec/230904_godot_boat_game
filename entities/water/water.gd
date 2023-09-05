f990fec46dad9201ee401f6eb628ea5b652063b2
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-09-05 20:40:05 +0100

    Depth shit

--- entities/water/water.gd
+++ entities/water/water.gd
@@ -1,12 +1,21 @@
 class_name Water
 extends Node3D
 
+@export var depth_viewport: SubViewport
+
 @onready var mesh: MeshInstance3D = $MeshInstance3D
 
 var material: ShaderMaterial
 
 func _ready():
 	material = mesh.mesh.surface_get_material(0)
+	
+func _process(_delta: float) -> void:
+	var texture = depth_viewport.get_texture()
+	
+	print(texture.get_image().get_size())
+	var pixel = texture.get_image().get_pixel(1, 1)
+	print(pixel)
 	
 func get_height_at_position(world_position: Vector3) -> float:
 	return 0
