befad7d296d795876548d4d0be976fd0f43e89c7
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-09-06 19:42:25 +0100

    Sad

--- entities/ocean/ocean.gd
+++ entities/ocean/ocean.gd
@@ -1,5 +1,11 @@
 class_name Ocean
 extends Node3D
 
+@onready var simulation: SubViewport = $Simulation
+
 func get_height(position: Vector3) -> float:
+#	var texture = simulation.get_texture()
+#	var image = texture.get_image() # TOO SLOW
+	
+#	return image.get_pixel(10, 10).r
 	return 0
