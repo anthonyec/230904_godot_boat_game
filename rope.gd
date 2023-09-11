f64a23972c78e2bd732850d68ebf59824ebf4f92
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2023-09-11 23:44:05 +0100

    Change rope to be a spring technically

--- rope.gd
+++ rope.gd
@@ -7,19 +7,12 @@ extends Node
 @export var attachment_point_a: Node3D
 @export var attachment_point_b: Node3D
 @export var max_length: float = 10
-
-# Use this to make the rope less "tight" causing objects to be pull harshly.
-# Technically the value is how long for the object to reach the desired point.
-@export var stretchiness: float = 5
-@export var strength_multipler_a: float = 0.3
-@export var strength_multipler_b: float = 1
+@export var stiffness: float = 80
+@export var rigid: bool = false
 
 func _process(_delta: float) -> void:
 	if not enabled or not attachment_point_a or not attachment_point_b:
 		return
-		
-	if Debug.is_flag_enabled(Debug.Flag.ROPE):
-		DebugDraw.draw_line_3d(attachment_point_a.global_position, attachment_point_b.global_position, Color.BLACK)
 	
 	var point_a = attachment_point_a.global_position
 	var point_b = attachment_point_b.global_position
@@ -30,28 +23,17 @@ func _process(_delta: float) -> void:
 	var direction_a_to_b = point_a.direction_to(point_b)
 	var direction_b_to_a = point_b.direction_to(point_a)
 	
-	var desired_point_a = point_b + direction_b_to_a.normalized() * max_length
-	var desired_point_b = point_a + direction_a_to_b.normalized() * max_length
-	
 	var distance = point_a.distance_to(point_b)
 	
-	# Point B force.
-	if distance > max_length:
-		var acceleration: float = distance / sqrt(stretchiness)
+	var spring_force = stiffness * (max_length - distance)
+	var acceleration_a = spring_force / rigid_body_a.mass
+	var acceleration_b = spring_force / rigid_body_b.mass
+	var force_a = rigid_body_a.mass * acceleration_a
+	var force_b = rigid_body_b.mass * acceleration_b
+	
+	if not rigid and distance > max_length:
+		rigid_body_a.apply_force(direction_b_to_a * force_a, offset_a)
+		rigid_body_b.apply_force(direction_a_to_b * force_b, offset_b)
 		
-		# Probably can caclulate it like this, by having specfic numbers is 
-		# easier to tune right now.
-#		var strength_multipler: float = rigid_body_b.mass / rigid_body_a.mass
-		
-		var force_a: float = rigid_body_a.mass * acceleration * strength_multipler_a
-		var force_b: float = rigid_body_b.mass * acceleration * strength_multipler_b
-		
-		rigid_body_a.apply_force(direction_a_to_b * force_a, offset_a)
-		rigid_body_b.apply_force(direction_b_to_a * force_b, offset_b)
-		
-		if Debug.is_flag_enabled(Debug.Flag.ROPE):
-			DebugDraw.draw_cube(desired_point_a, 0.2, Color.BLUE)
-			DebugDraw.draw_ray_3d(point_a, direction_a_to_b, distance - max_length, Color.BLUE)
-			
-			DebugDraw.draw_cube(desired_point_b, 0.2, Color.RED)
-			DebugDraw.draw_ray_3d(point_b, direction_b_to_a, distance - max_length, Color.RED)
+	if Debug.is_flag_enabled(Debug.Flag.ROPE):
+		DebugDraw.draw_line_3d(attachment_point_a.global_position, attachment_point_b.global_position, Color.BLACK)
